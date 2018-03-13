39135923e6952fb698361bc4e67ae23dfa2b7963
Noah Wolfe <noahwolfe1999@gmail.com>
2018-03-13 19:54:09 -0400

    Added tile and map, as wel as render_all function!

--- firstrl.py
+++ firstrl.py
@@ -3,8 +3,38 @@ import libtcodpy as libtcod
 #actual size of the window
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
-
 LIMIT_FPS = 20  #20 frames-per-second maximum
+
+
+# map sizing
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
+
+# map colors
+color_dark_wall = libtcod.Color(0, 0, 100)
+color_dark_ground = libtcod.Color(50, 50, 150)
+
+# tile class
+class Tile:
+    def __init__(self, blocked, block_sight = None):
+        self.blocked = blocked
+        # if a tile is blocked, it also blocks sight
+        if block_sight is None: block_sight = blocked
+        self.block_sight = block_sight
+
+# make the map!
+def make_map():
+    global map
+    map = [ [ Tile(False)
+        for y in range(MAP_HEIGHT) ]
+            for x in range(MAP_WIDTH) ]
+
+    # add some testing "pillars"
+    map[30][22].blocked = True
+    map[30][22].block_sight = True
+    map[50][22].blocked = True
+    map[50][22].block_sight = True
+
 
 class Object:
     # constructor
@@ -15,8 +45,10 @@ class Object:
         self.color = color
 
     def move(self, dx, dy):
-        self.x += dx
-        self.y += dy
+        # check if the tile we're stepping onto is blocked or not
+        if not map[self.x + dx][self.y + dy].blocked:
+            self.x += dx
+            self.y += dy
 
     def draw(self):
         libtcod.console_set_default_foreground(con, self.color)
@@ -66,15 +98,37 @@ player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
 npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
 objects = [npc, player]
 
+def render_all():
+    # draw all objects!
+    for object in objects:
+        object.draw()
+
+    # draw all tiles!
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
+            wall = map[x][y].block_sight # if block_sight is true, it's a "wall"
+            if wall:
+                libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET)
+            else:
+                libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET)
+
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
+# make sure to make the map before the game begins!
+make_map()
+
 while not libtcod.console_is_window_closed():
 
     #libtcod.console_set_default_foreground(con, libtcod.white)
     #libtcod.console_put_char(con, playerx, playery, '@', libtcod.BKGND_NONE)
 
-    for object in objects:
-        object.draw()
+    #for object in objects:
+    #    object.draw()
 
-    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+    #libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
+    render_all()
+
     libtcod.console_flush()
 
     #libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE)
